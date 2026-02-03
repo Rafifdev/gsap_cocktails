ec7cf34893c25131729b28b25ed708af1ed76394
Rafif <merafif28@gmail.com>
2026-02-03 22:15:19 +0700

    add : Hero scroll  animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,13 @@
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef();
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars, words" });
     const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
@@ -25,8 +30,9 @@ const Hero = () => {
       stagger: 0.05,
       delay: 1,
     });
- 
-    gsap.timeline({
+
+    gsap
+      .timeline({
         scrollTrigger: {
           trigger: "#hero",
           start: "top top",
@@ -34,8 +40,28 @@ const Hero = () => {
           scrub: true,
         },
       })
-      .to(".right-leaf", { y: 200 }, 0) 
-      .to(".left-leaf", { y: -200 }, 0)
+      .to(".right-leaf", { y: 200 }, 0)
+      .to(".left-leaf", { y: -200 }, 0);
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% 50%" : "bottom top";
+
+    // Video timeline animation
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: 'video',
+            start: startValue,
+            end: endValue,
+            scrub: true,
+            pin: true,
+        }
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+        tl.to(videoRef.current, {
+            currentTime: videoRef.current.duration
+        })
+    }
   }, []);
 
   return (
@@ -74,6 +100,16 @@ const Hero = () => {
           </div>
         </div>
       </section>
+
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        ></video>
+      </div>
     </>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,17 +28,34 @@ const Navbar = () => {
         ease: "power1.inOut",
       },
     );
+
+    gsap.from(".home", {
+      opacity: 0,
+      xPercent: -100,
+      delay: 1,
+      duration: 1.8,
+      ease: "expo.out",
+    })
+
+    gsap.from(".navlink", {
+      opacity: 0,
+      xPercent: 100,
+      delay: 1,
+      duration: 1.8,
+      ease: "expo.out",
+    })
+    
   });
   return (
     <nav>
       <div className="lg:px-10">
-        <a href="#home" className="flex items-center gap-2">
+        <a href="#home" className="flex items-center gap-2 home">
           <img src="/images/logo.png" alt="logo" />
           <p>Velvet Pour</p>
         </a>
         <ul>
           {navLinks.map((link: NavLinks) => (
-            <li key={link.id}>
+            <li key={link.id} className="navlink">
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
